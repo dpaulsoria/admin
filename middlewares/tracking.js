02fbd18da8d13833f216ee7f7a089023293835af
Cliente CONTROL-PC <CltControl@GGIECLUGWRK175.espol.edu.ec>
2023-01-11 13:51:59 -0500

    Cambiando hasta hito2-admin

--- middlewares/tracking.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var tracking = (req, res, next) => {
-  res.cookie('tracing', req._parsedOriginalUrl.path , {expire : new Date() + 9999});
-  return next();
-};
-module.exports = tracking;
